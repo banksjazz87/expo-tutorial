f23ad9bc35bbb9e8ceb661dc916df006b3da1975
Chris <banksjazz87@gmail.com>
2024-05-09 13:17:55 -0400

    feature: adding the icons

--- App.js
+++ App.js
@@ -4,11 +4,22 @@ import { useState } from "react";
 import ImageViewer from "./components/ImageViewer.js";
 import Button from "./components/Button.js";
 import * as ImagePicker from "expo-image-picker";
+import CircleButton from "./components/CircleButton.js";
+import IconButton from "./components/IconButton.js";
 
 const PlaceHolderImage = require("./assets/images/background-image.png");
 
 export default function App() {
 	const [selectedImage, setSelectedImage] = useState(null);
+	const [showAppOptions, setShowAppOptions] = useState(false);
+
+	const onReset = () => {
+		setShowAppOptions(false);
+	};
+
+	const onAddSticker = () => {};
+
+	const onSaveImageAsync = async () => {};
 
 	const pickImageAsync = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
@@ -18,6 +29,7 @@ export default function App() {
 
 		if (!result.canceled) {
 			setSelectedImage(result.assets[0].uri);
+			setShowAppOptions(true);
 		} else {
 			alert("You did not select any image.");
 		}
@@ -31,19 +43,37 @@ export default function App() {
 					imageSource={selectedImage}
 				/>
 			</View>
+			{showAppOptions ? (
+				<View style={styles.optionsContainer}>
+					<View style={styles.optionsRow}>
+						<IconButton
+							icon="refresh"
+							label="Reset"
+							onPress={onReset}
+						/>
+						<CircleButton onPress={onAddSticker} />
+						<IconButton
+							icon="save-alt"
+							label="Save"
+							onPress={onSaveImageAsync}
+						/>
+					</View>
+				</View>
+			) : (
+				<View style={styles.footerContainer}>
+					<Button
+						theme="primary"
+						label="Choose a photo"
+						onPress={pickImageAsync}
+					/>
+					<Button
+						theme=""
+						label="Use this photo"
+						onPress={() => setShowAppOptions(true)}
+					/>
+				</View>
+			)}
 
-			<View style={styles.footerContainer}>
-				<Button
-					theme="primary"
-					label="Choose a photo"
-					onPress={pickImageAsync}
-				/>
-				<Button
-					theme=""
-					label="Use this photo"
-					onPress={pickImageAsync}
-				/>
-			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -63,5 +93,14 @@ const styles = StyleSheet.create({
 	footerContainer: {
 		flex: 1 / 3,
 		alignItems: "center",
+	},
+
+	optionsContainer: {
+		position: "absolute",
+		bottom: 80,
+	},
+	optionsRow: {
+		alignItems: "center",
+		flexDirection: "row",
 	},
 });
---
+++ components/IconButton.js
@@ -0,0 +1,29 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+export default function IconButton({ icon, label, onPress }) {
+	return (
+		<Pressable
+			style={styles.iconButtons}
+			onPress={onPress}
+		>
+			<MaterialIcons
+				name={icon}
+				size={24}
+				color="#fff"
+			/>
+			<Text style={styles.iconButtonLabel}>{label}</Text>
+		</Pressable>
+	);
+}
+
+const styles = StyleSheet.create({
+	iconButtons: {
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	iconButtonLabel: {
+		color: "#fff",
+		marginTop: 12,
+	},
+});
